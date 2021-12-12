1398888640aa3d489e4d7f119b6f8b6ad536bde6
Muhammad Fadlyas <mhmmdfdlyas@gmail.com>
2021-12-12 10:21:01 +0700

    Create tg_upload.sh

---
+++ tg_upload.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Copyright (C) 2020 Muhammad Fadlyas (fadlyas07)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Function kanged from laststandrighthere's scripts
+tg()
+{
+    ACTION=$1
+    EXTRA=$2
+    CHANNEL_ID=$3
+    CAPTION=$4
+    URL="https://api.telegram.org/bot${telegram_bot_token}"
+
+    case "$ACTION" in
+        msg)
+            curl \
+            -d chat_id=$CHANNEL_ID \
+            -d parse_mode=HTML \
+            -d disable_web_page_preview=true \
+            -X POST ${URL}/sendMessage \
+            -d text="$EXTRA"
+            ;;
+        file)
+            curl \
+            -F chat_id=$CHANNEL_ID \
+            -d parse_mode=HTML \
+            -F disable_web_page_preview=true \
+            -F document=@$EXTRA ${URL}/sendDocument \
+            -F caption="$CAPTION"
+            ;;
+    esac
+}
