4cb32a4bd702562c9913f3b7fe06496c6613d7cb
Muhammad Fadlyas <mhmmdfdlyas@gmail.com>
2021-12-12 10:50:20 +0700

    Create kernel_env.sh

---
+++ kernel_env.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Copyright (C) 2020 Muhammad Fadlyas (fadlyas07)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+export ARCH=arm64
+export SUBARCH=$ARCH
+export telegram_channel_id=$1
+export telegram_bot_token=$2
+export KBUILD_BUILD_USER=greenforce-tesd
+export KBUILD_BUILD_HOST=weekly-build
+
+info()
+{
+    kernel_version=$(cat $(pwd)/out/.config | grep Linux/arm64 | cut -d " " -f3)
+    uts_version=$(cat $(pwd)/out/include/generated/compile.h | grep UTS_VERSION | cut -d '"' -f2)
+    toolchain_version=$(cat $(pwd)/out/include/generated/compile.h | grep LINUX_COMPILER | cut -d '"' -f2)
+    trigger_sha="$(git rev-parse HEAD)"
+    latest_commit="$(git log --pretty=format:'%s' -1)"
+    commit_by=$(git log --pretty=format:'by %an' -1)
+}
