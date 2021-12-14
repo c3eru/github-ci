9a55d26379e3b25737361bf38ee5232d2be89896
Muhammad Fadlyas <mhmmdfdlyas@gmail.com>
2021-12-14 12:42:30 +0700

    Update tg_upload.sh

--- tg_upload.sh
+++ tg_upload.sh
@@ -2,41 +2,32 @@
 # Copyright (C) 2020 Muhammad Fadlyas (fadlyas07)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Function kanged from laststandrighthere's scripts
-tg()
+URL="https://api.telegram.org/bot${token_bot}/"
+file ()
 {
-    ACTION=$1
-    EXTRA=$2
-    CHANNEL_ID=$3
-    CAPTION=$4
-    URL="https://api.telegram.org/bot${telegram_bot_token}"
-
-    case "$ACTION" in
-        msg)
-            curl \
-            -d chat_id=$CHANNEL_ID \
-            -d parse_mode=HTML \
-            -d disable_web_page_preview=true \
-            -X POST ${URL}/sendMessage \
-            -d text="$EXTRA"
-            ;;
-        file)
-            curl \
-            -F chat_id=$CHANNEL_ID \
-            -F parse_mode=HTML \
-            -F disable_web_page_preview=true \
-            -F document=@$EXTRA ${URL}/sendDocument \
-            -F caption="$CAPTION"
-            ;;
-    esac
+    curl \
+    -F "disable_web_page_preview=true" \
+    -F "parse_mode=html" \
+    -F document=@$1 $URLsendDocument \
+    -F chat_id=$2
 }
 
-notif()
+msg ()
+{
+    curl \
+    -d "disable_web_page_preview=true" \
+    -d "parse_mode=html" \
+    -X POST $URLsendMessage \
+    -d chat_id=$1 \
+    -d text=$2
+}
+
+notif ()
 {
     echo -e "\e[1;32m$*\e[0m"
 }
 
-err()
+err ()
 {
     echo -e "\e[1;41m$*\e[0m"
 }
